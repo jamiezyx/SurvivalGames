28153baa5192209d5810f0173130ffaa4403ed49
jamiezyx <romari123456789@gmail.com>
2016-02-05 04:44:56 -0500

    Main.php
    I changed the name the pocket sg.

--- SurvivalGames/Main.php
+++ SurvivalGames/Main.php
@@ -1,1 +1,346 @@
+<?php
 
+namespace PocketSurvivalgames;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\Player;
+use pocketmine\block\Block;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\item\Item;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\entity\Effect;
+use pocketmine\tile\Chest;
+use pocketmine\inventory\ChestInventory;
+
+class SurvivalGamesV2 extends PluginBase implements Listener {
+
+    public $prefix = TextFormat::GRAY . "[" . TextFormat::WHITE . TextFormat::BOLD . "S" . TextFormat::RED . "G" . TextFormat::RESET . TextFormat::GRAY . "] ";
+	public $mode = 0;
+	public $arenas = array();
+	public $currentLevel = "";
+	
+	public function onEnable()
+	{
+        $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+		@mkdir($this->getDataFolder());
+		$config2 = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
+		$config2->save();
+		$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+		if($config->get("arenas")!=null)
+		{
+			$this->arenas = $config->get("arenas");
+		}
+		foreach($this->arenas as $lev)
+		{
+			$this->getServer()->loadLevel($lev);
+		}
+		$items = array(array(261,0,1),array(262,0,2),array(262,0,3),array(267,0,1),array(268,0,1),array(272,0,1),array(276,0,1),array(283,0,1));
+		if($config->get("chestitems")==null)
+		{
+			$config->set("chestitems",$items);
+		}
+		$config->save();
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new GameSender($this), 20);
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new RefreshSigns($this), 10);
+	}
+	
+	public function onMove(PlayerMoveEvent $event)
+	{
+		$player = $event->getPlayer();
+		$level = $player->getLevel()->getFolderName();
+		if(in_array($level,$this->arenas))
+		{
+			$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+			$sofar = $config->get($level . "StartTime");
+			if($sofar > 0)
+			{
+				$to = clone $event->getFrom();
+				$to->yaw = $event->getTo()->yaw;
+				$to->pitch = $event->getTo()->pitch;
+				$event->setTo($to);
+			}
+		}
+	}
+	
+	public function onLogin(PlayerLoginEvent $event)
+	{
+		$player = $event->getPlayer();
+		$player->getInventory()->clearAll();
+		$spawn = $this->getServer()->getDefaultLevel()->getSafeSpawn();
+		$this->getServer()->getDefaultLevel()->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+		$player->teleport($spawn,0,0);
+	}
+	
+	public function onBlockBreak(BlockBreakEvent $event)
+	{
+		$player = $event->getPlayer();
+		$level = $player->getLevel()->getFolderName();
+		if(in_array($level,$this->arenas))
+		{
+			$event->setCancelled(true);
+		}
+	}
+	
+	public function onBlockPlace(BlockPlaceEvent $event)
+	{
+		$player = $event->getPlayer();
+		$level = $player->getLevel()->getFolderName();
+		if(in_array($level,$this->arenas))
+		{
+			$event->setCancelled(true);
+		}
+	}
+	
+	public function onCommand(CommandSender $player, Command $cmd, $label, array $args) {
+        switch($cmd->getName()){
+			case "sg":
+				if($player->isOp())
+				{
+					if(!empty($args[0]))
+					{
+						if($args[0]=="make")
+						{
+							if(!empty($args[1]))
+							{
+								if(file_exists($this->getServer()->getDataPath() . "/worlds/" . $args[1]))
+								{
+									$this->getServer()->loadLevel($args[1]);
+									$this->getServer()->getLevelByName($args[1])->loadChunk($this->getServer()->getLevelByName($args[1])->getSafeSpawn()->getFloorX(), $this->getServer()->getLevelByName($args[1])->getSafeSpawn()->getFloorZ());
+									array_push($this->arenas,$args[1]);
+									$this->currentLevel = $args[1];
+									$this->mode = 1;
+									$player->sendMessage($this->prefix . "You are about to register an arena. Tap a block to set a spawn point there!");
+									$player->setGamemode(1);
+									$player->teleport($this->getServer()->getLevelByName($args[1])->getSafeSpawn(),0,0);
+								}
+								else
+								{
+									$player->sendMessage($this->prefix . "There is no world with this name.");
+								}
+							}
+							else
+							{
+								$player->sendMessage($this->prefix . "Missing parameters.");
+							}
+						}
+						else
+						{
+							$player->sendMessage($this->prefix . "There is no such command.");
+						}
+					}
+					else
+					{
+						$player->sendMessage($this->prefix . "Missing parameters.");
+					}
+				}
+			return true;
+			case "setrank":
+				if($player->isOp())
+				{
+				if(!empty($args[0]))
+				{
+					if(!empty($args[1]))
+					{
+					$rank = "";
+					if($args[0]=="VIP+")
+					{
+						$rank = "Â§b[Â§aVIPÂ§4+Â§b]";
+					}
+					else if($args[0]=="YouTuber")
+					{
+						$rank = "Â§b[Â§4YouÂ§7TuberÂ§b]";
+					}
+					else if($args[0]=="YouTuber+")
+					{
+						$rank = "Â§b[Â§4YouÂ§7TuberÂ§4+Â§b]";
+					}
+					else
+					{
+						$rank = "Â§b[Â§a" . $args[0] . "Â§b]";
+					}
+					$config = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
+					$config->set($args[1],$rank);
+					$config->save();
+					$player->sendMessage($args[1] . " got this rank: " . $rank);
+					}
+					else
+					{
+						$player->sendMessage("Missing parameter(s)");
+					}
+				}
+				else
+				{
+					$player->sendMessage("Missing parameter(s)");
+				}
+				}
+			return true;
+		}
+	}
+	
+	public function onChat(PlayerChatEvent $event)
+	{
+		$player = $event->getPlayer();
+		$message = $event->getMessage();
+		$config = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
+		$rank = "";
+		if($config->get($player->getName()) != null)
+		{
+			$rank = $config->get($player->getName());
+		}
+		$event->setFormat($rank . TextFormat::WHITE . $player->getName() . " Â§d:Â§f " . $message);
+	}
+	
+	public function onInteract(PlayerInteractEvent $event)
+	{
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		$tile = $player->getLevel()->getTile($block);
+		
+		if($tile instanceof Sign) 
+		{
+			if($this->mode==26)
+			{
+				$tile->setText(TextFormat::AQUA . "[Join]",TextFormat::YELLOW  . "0 / 24",$this->currentLevel,$this->prefix);
+				$this->refreshArenas();
+				$this->currentLevel = "";
+				$this->mode = 0;
+				$player->sendMessage($this->prefix . "The arena has been registered successfully!");
+			}
+			else
+			{
+				$text = $tile->getText();
+				if($text[3] == $this->prefix)
+				{
+					if($text[0]==TextFormat::AQUA . "[Join]")
+					{
+						$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+						$level = $this->getServer()->getLevelByName($text[2]);
+						$aop = count($level->getPlayers());
+						$thespawn = $config->get($text[2] . "Spawn" . ($aop+1));
+						$spawn = new Position($thespawn[0]+0.5,$thespawn[1],$thespawn[2]+0.5,$level);
+						$level->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+						$player->teleport($spawn,0,0);
+						$player->setNameTag($player->getName());
+						$player->getInventory()->clearAll();
+						$config2 = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
+						$rank = $config2->get($player->getName());
+						if($rank == "Â§b[Â§aVIPÂ§4+Â§b]")
+						{
+							$player->getInventory()->setContents(array(Item::get(0, 0, 0)));
+							$player->getInventory()->setHelmet(Item::get(Item::CHAIN_HELMET));
+							$player->getInventory()->setChestplate(Item::get(Item::CHAIN_CHESTPLATE));
+							$player->getInventory()->setLeggings(Item::get(Item::CHAIN_LEGGINGS));
+							$player->getInventory()->setBoots(Item::get(Item::CHAIN_BOOTS));
+							$player->getInventory()->setItem(0, Item::get(Item::DIAMOND_AXE, 0, 1));
+							$player->getInventory()->setHotbarSlotIndex(0, 0);
+						}
+						else if($rank == "Â§b[Â§aVIPÂ§b]")
+						{
+							$player->getInventory()->setContents(array(Item::get(0, 0, 0)));
+							$player->getInventory()->setHelmet(Item::get(Item::GOLD_HELMET));
+							$player->getInventory()->setChestplate(Item::get(Item::GOLD_CHESTPLATE));
+							$player->getInventory()->setLeggings(Item::get(Item::LEATHER_PANTS));
+							$player->getInventory()->setBoots(Item::get(Item::LEATHER_BOOTS));
+							$player->getInventory()->setItem(0, Item::get(Item::IRON_AXE, 0, 1));
+							$player->getInventory()->setHotbarSlotIndex(0, 0);
+						}
+						else if($rank == "Â§b[Â§4YouÂ§7TuberÂ§b]")
+						{
+							$player->getInventory()->setContents(array(Item::get(0, 0, 0)));
+							$player->getInventory()->setHelmet(Item::get(Item::GOLD_HELMET));
+							$player->getInventory()->setChestplate(Item::get(Item::GOLD_CHESTPLATE));
+							$player->getInventory()->setLeggings(Item::get(Item::GOLD_LEGGINGS));
+							$player->getInventory()->setBoots(Item::get(Item::GOLD_BOOTS));
+							$player->getInventory()->setItem(0, Item::get(Item::IRON_AXE, 0, 1));
+							$player->getInventory()->setHotbarSlotIndex(0, 0);
+						}
+						else if($rank == "Â§b[Â§aVIPÂ§b]")
+						{
+							$player->getInventory()->setContents(array(Item::get(0, 0, 0)));
+							$player->getInventory()->setHelmet(Item::get(Item::DIAMOND_HELMET));
+							$player->getInventory()->setChestplate(Item::get(Item::CHAIN_CHESTPLATE));
+							$player->getInventory()->setLeggings(Item::get(Item::CHAIN_LEGGINGS));
+							$player->getInventory()->setBoots(Item::get(Item::DIAMOND_BOOTS));
+							$player->getInventory()->setItem(0, Item::get(Item::DIAMOND_AXE, 0, 1));
+							$player->getInventory()->setHotbarSlotIndex(0, 0);
+						}
+					}
+					else
+					{
+						$player->sendMessage($this->prefix . "You can not join this match.");
+					}
+				}
+			}
+		}
+		else if($this->mode>=1&&$this->mode<=24)
+		{
+			$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+			$config->set($this->currentLevel . "Spawn" . $this->mode, array($block->getX(),$block->getY()+2,$block->getZ()));
+			$player->sendMessage($this->prefix . "Spawn " . $this->mode . " has been registered!");
+			$this->mode++;
+			if($this->mode==25)
+			{
+				$player->sendMessage($this->prefix . "Now tap on a deathmatch spawn.");
+			}
+			$config->save();
+		}
+		else if($this->mode==25)
+		{
+			$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+			$level = $this->getServer()->getLevelByName($this->currentLevel);
+			$level->setSpawn(new Vector3($block->getX(),$block->getY()+2,$block->getZ()));
+			$config->set("arenas",$this->arenas);
+			$player->sendMessage($this->prefix . "You've been teleported back. Tap a sign to register it for the arena!");
+          $spawn = $this->getServer()->getDefaultLevel()->getSafeSpawn();
+			$this->getServer()->getDefaultLevel()->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+			$player->teleport($spawn,0,0);
+			$config->save();
+			$this->mode=26;
+		}
+	}
+	
+	public function refreshArenas()
+	{
+		$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+		$config->set("arenas",$this->arenas);
+		foreach($this->arenas as $arena)
+		{
+			$config->set($arena . "PlayTime", 780);
+			$config->set($arena . "StartTime", 60);
+		}
+		$config->save();
+	}
+}
+
+class RefreshSigns extends PluginTask {
+    public $prefix = TextFormat::GRAY . "[" . TextFormat::WHITE . TextFormat::BOLD . "S" . TextFormat::RED . "G" . TextFormat::RESET . TextFormat::GRAY . "] ";
+	public function __construct($plugin)
+	{
+		$this->plugin = $plugin;
+		parent::__construct($plugin);
+	}
+  
+	public function onRun($tick)
+	{
+		$allplayers = $this->plugin->getServer()->getOnlinePlayers();
+		$level = $this->plugin->getServer()->getDefaultLevel();
+		$tiles = $level->getTiles();
+		foreach($tiles as $t) {
+			if($t instanceof Sign) {	
+				$text = $t->getText();
+				if($text[3]==$this->prefix)
