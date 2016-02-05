7f484f33bc755135556fbda85083afd13498b3e3
jamiezyx <romari123456789@gmail.com>
2016-02-05 04:47:54 -0500

    Main.php
    Sg.

--- SurvivalGames/Main.php
+++ SurvivalGames/Main.php
@@ -343,4 +343,212 @@ class RefreshSigns extends PluginTask {
 		foreach($tiles as $t) {
 			if($t instanceof Sign) {	
 				$text = $t->getText();
-				if($text[3]==$this->prefix)
+				if($text[3]==$this->prefix){
+					$aop = 0;
+					foreach($allplayers as $player){if($player->getLevel()->getFolderName()==$text[2]){$aop=$aop+1;}}
+					$ingame = TextFormat::AQUA . "[Join]";
+					$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+					if($config->get($text[2] . "PlayTime")!=780)
+					{
+						$ingame = TextFormat::DARK_PURPLE . "[Running]";
+					}
+					else if($aop>=24)
+					{
+						$ingame = TextFormat::GOLD . "[Full]";
+					}
+					$t->setText($ingame,TextFormat::YELLOW  . $aop . " / 24",$text[2],$this->prefix);
+				}
+			}
+		}
+	}
+}
+
+class GameSender extends PluginTask {
+    public $prefix = TextFormat::GRAY . "[" . TextFormat::WHITE . TextFormat::BOLD . "S" . TextFormat::RED . "G" . TextFormat::RESET . TextFormat::GRAY . "] ";
+	public function __construct($plugin)
+	{
+		$this->plugin = $plugin;
+		parent::__construct($plugin);
+	}
+  
+	public function onRun($tick)
+	{
+		$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+		$arenas = $config->get("arenas");
+		if(!empty($arenas))
+		{
+			foreach($arenas as $arena)
+			{
+				$time = $config->get($arena . "PlayTime");
+				$timeToStart = $config->get($arena . "StartTime");
+				$levelArena = $this->plugin->getServer()->getLevelByName($arena);
+				if($levelArena instanceof Level)
+				{
+					$playersArena = $levelArena->getPlayers();
+					if(count($playersArena)==0)
+					{
+						$config->set($arena . "PlayTime", 780);
+						$config->set($arena . "StartTime", 60);
+					}
+					else
+					{
+						if(count($playersArena)>=2)
+						{
+							if($timeToStart>0)
+							{
+								$timeToStart--;
+								foreach($playersArena as $pl)
+								{
+									$pl->sendPopup(TextFormat::GOLD . $timeToStart . " seconds");
+								}
+								if($timeToStart<=0)
+								{
+									$this->refillChests($levelArena);
+								}
+								$config->set($arena . "StartTime", $timeToStart);
+							}
+							else
+							{
+								$aop = count($levelArena->getPlayers());
+								if($aop==1)
+								{
+									foreach($playersArena as $pl)
+									{
+										$pl->sendMessage($this->prefix . TextFormat::GREEN . "You won!");
+										$pl->getInventory()->clearAll();
+										$pl->removeAllEffects();
+										$pl->setNameTag($pl->getName());
+										$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+										$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+										$pl->teleport($spawn,0,0);
+									}
+									$config->set($arena . "PlayTime", 780);
+									$config->set($arena . "StartTime", 60);
+								}
+								$time--;
+								if($time>=180)
+								{
+								$time2 = $time - 180;
+								$minutes = $time2 / 60;
+								if(is_int($minutes) && $minutes>0)
+								{
+									foreach($playersArena as $pl)
+									{
+										$pl->sendMessage($this->prefix . $minutes . " minutes to deathmatch");
+									}
+								}
+								else if($time2 == 300)
+								{
+									foreach($playersArena as $pl)
+									{
+										$pl->sendMessage($this->prefix . "The chests have been refilled!");
+									}
+									$this->refillChests($levelArena);
+								}
+								else if($time2 == 30 || $time2 == 15 || $time2 == 10 || $time2 ==5 || $time2 ==4 || $time2 ==3 || $time2 ==2 || $time2 ==1)
+								{
+									foreach($playersArena as $pl)
+									{
+										$pl->sendMessage($this->prefix . $time2 . " seconds to deathmatch");
+									}
+								}
+								if($time2 <= 0)
+								{
+									$spawn = $levelArena->getSafeSpawn();
+									$levelArena->loadChunk($spawn->getX(), $spawn->getZ());
+									foreach($playersArena as $pl)
+									{
+										$pl->teleport($spawn,0,0);
+									}
+								}
+								}
+								else
+								{
+									$minutes = $time / 60;
+									if(is_int($minutes) && $minutes>0)
+									{
+										foreach($playersArena as $pl)
+										{
+											$pl->sendMessage($this->prefix . $minutes . " minutes remaining");
+										}
+									}
+									else if($time == 30 || $time == 15 || $time == 10 || $time ==5 || $time ==4 || $time ==3 || $time ==2 || $time ==1)
+									{
+										foreach($playersArena as $pl)
+										{
+											$pl->sendMessage($this->prefix . $time . " seconds remaining");
+										}
+									}
+									if($time <= 0)
+									{
+										$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+										$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+										foreach($playersArena as $pl)
+										{
+											$pl->teleport($spawn,0,0);
+											$pl->sendMessage($this->prefix . "No winner this time!");
+											$pl->getInventory()->clearAll();
+										}
+										$time = 780;
+									}
+								}
+								$config->set($arena . "PlayTime", $time);
+							}
+						}
+						else
+						{
+							if($timeToStart<=0)
+							{
+								foreach($playersArena as $pl)
+								{
+									$pl->sendMessage($this->prefix . TextFormat::GREEN . "You won!");
+									$pl->getInventory()->clearAll();
+									$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+									$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+									$pl->teleport($spawn);
+								}
+								$config->set($arena . "PlayTime", 780);
+								$config->set($arena . "StartTime", 60);
+							}
+							else
+							{
+								foreach($playersArena as $pl)
+								{
+									$pl->sendPopup(TextFormat::RED . "More players needed!");
+								}
+								$config->set($arena . "PlayTime", 780);
+								$config->set($arena . "StartTime", 60);
+							}
+						}
+					}
+				}
+			}
+		}
+		$config->save();
+	}
+	
+	public function refillChests(Level $level)
+	{
+		$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+		$tiles = $level->getTiles();
+		foreach($tiles as $t) {
+			if($t instanceof Chest) 
+			{
+				$chest = $t;
+				$chest->getInventory()->clearAll();
+				if($chest->getInventory() instanceof ChestInventory)
+				{
+					for($i=0;$i<=26;$i++)
+					{
+						$rand = rand(1,3);
+						if($rand==1)
+						{
+							$k = array_rand($config->get("chestitems"));
+							$v = $config->get("chestitems")[$k];
+							$chest->getInventory()->setItem($i, Item::get($v[0],$v[1],$v[2]));
+						}
+					}									
+				}
+			}
+		}
+	}
